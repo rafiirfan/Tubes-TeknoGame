649e0770718bbcc4a9f9a16898f2024868c32ed1
bagasyah <bagassyah18@gmail.com>
2023-05-21 22:18:54 +0700

    fitur pause di game play

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+    {
+    public GameObject panelPause;
+    public void PauseControl(){
+        if (Time.timeScale == 1){
+            panelPause.SetActive (true);
+            Time.timeScale = 0;
+        }else {
+            Time.timeScale = 1;
+            panelPause.SetActive (false);
+        }
+    }
+    public void Restart (){
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+    public void menu(){
+        Application.LoadLevel(0);
+    }
+}
+
